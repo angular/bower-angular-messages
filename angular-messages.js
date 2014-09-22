331e5b9f22c562eeffc0e6350121485048401c68
jenkins <ci-bot@angularjs.org>
2014-09-22 15:36:36 -0700

    v1.3.0-build.3288+sha.f5bb34a

--- angular-messages.js
+++ angular-messages.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3287+sha.4b83f6c
+ * @license AngularJS v1.3.0-build.3288+sha.f5bb34a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
