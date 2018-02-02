85504bd0d625a3860f0afbdbe351a8befc4b9568
jenkins <ci-bot@angularjs.org>
2018-02-02 05:13:25 -0800

    v1.6.9

--- angular-messages.js
+++ angular-messages.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.9-build.5550+sha.16b82c6
+ * @license AngularJS v1.6.9
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -272,7 +272,7 @@ angular.module('ngMessages', [], function initAngularHelpers() {
   isString = angular.isString;
   jqLite = angular.element;
 })
-  .info({ angularVersion: '1.6.9-build.5550+sha.16b82c6' })
+  .info({ angularVersion: '1.6.9' })
 
   /**
    * @ngdoc directive
